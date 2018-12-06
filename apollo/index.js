34652425e7bfe324a7dfa36e416c1408e637ff36
Luke Burns <lukemburns@gmail.com>
2018-12-06 17:18:26 -0500

    init @now/node

--- apollo/index.js
+++ apollo/index.js
@@ -1,32 +1,19 @@
-const express = require("express");
-const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServer, gql } = require('apollo-server-micro')
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
   }
-`;
+`
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => "Hello world from ApolloServer on Now 2.0!"
+    hello: () => 'Hello world from ApolloServer on Now 2.0!'
   }
-};
+}
 
-const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true })
 
-const app = express();
-server.applyMiddleware({ app });
-app.get("/", (req, res) => {
-  res.redirect("/graphql");
-});
-
-const port = 4000;
-
-app.listen({ port }, () =>
-  console.log(
-    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
-  )
-);
+module.exports = server.createHandler()
