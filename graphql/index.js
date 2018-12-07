b0d08c59d686264b4a62db0d882ad895b995f089
Luke Burns <lukemburns@gmail.com>
2018-12-07 15:56:47 -0500

    separate out merge

--- graphql/index.js
+++ graphql/index.js
@@ -1,10 +1,8 @@
-const graphqlHTTP = require('express-graphql')
 const schema = require('./schema')
-
-const handler = graphqlHTTP({
-  schema: buildSchema(schema.types),
-  rootValue: schema.resolvers,
-  graphiql: true
-})
-
-module.exports = handler
+const { graphql, buildSchema } = require('graphql')
+module.exports = function (req, res) {
+  const qs = require('url').parse(req.url, true).query
+  graphql(buildSchema(schema.types), qs.query, schema.resolvers).then(result => {
+    res.end(JSON.stringify(result))
+  })
+}
